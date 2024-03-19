83b98b1559e5196c5bff557628d8fa527b6fa275
Pei Mo <pei@luna.inf.ed.ac.uk>
2024-03-19 20:06:13 +0000

    upload run tpch scripts

--- measure/run_tpch.sh
+++ measure/run_tpch.sh
@@ -10,8 +10,23 @@ echo -ne ".read tpch_schema.sql" | duckdb ./tpch.db
 for table in region nation part supplier partsupp customer orders lineitem
 do
   echo "duckdb load table from ${table}.tbl"
-  command="insert into ${table} select * from read_csv('/home/pei/Project/benchmarks/tpch-postgres/dbgen/out/${table}.tbl');"
+  command="copy ${table} from '/home/pei/Project/benchmarks/tpch-postgres/dbgen/out/${table}.tbl';"
   echo $command
   echo -ne "${command}" | duckdb ./tpch.db
 done
 
+# execute queries
+dir="/home/pei/Project/benchmarks/tpch-postgres/dbgen/out/skinner_explained"
+iteration=10
+
+rm -rf tpch_result/
+mkdir -p tpch_result/
+
+for i in $(eval echo {1.."${iteration}"}); do
+  for sql in "${dir}"/*; do
+    echo "execute ${sql}" 2>&1|tee -a skinner_explained_tpch-1_${i}.txt;
+    echo -ne ".read ${sql}" | duckdb ./tpch.db 2>&1|tee -a skinner_explained_tpch-1_${i}.txt;
+  done
+done
+
+mv skinner_explained_tpch-1_* tpch_result/.
