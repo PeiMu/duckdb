f8addb1d0ea208dea7b4dcd8ce99fb79a4cf9108
pei <ds1231h@gmail.com>
2024-07-10 16:05:55 +0100

    add config macros in Makefile and CMakelists

--- measure/measure.sh
+++ measure/measure.sh
@@ -1,2 +1,14 @@
-bash ./run_tpch.sh
-bash ./run_jcch.sh
+#!/bin/bash
+
+rm -rf job_result/
+mkdir -p job_result/
+rm -rf compile.log
+
+echo "official" 2>&1|tee -a compile.log
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=0 VERBOSE=1 make 2>&1|tee -a compile.log && cd measure && bash ./hyperfine_in_mem.sh official nan
+
+echo "query_split with join_order_optimization after query_split" 2>&1|tee -a compile.log
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 VERBOSE=1 make 2>&1|tee -a compile.log && cd measure && bash ./hyperfine_in_mem.sh query_split jop_after
+
+echo "query_split with join_order_optimization before query_split" 2>&1|tee -a compile.log
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 VERBOSE=1 make 2>&1|tee -a compile.log && cd measure && bash ./hyperfine_in_mem.sh query_split jop_before
