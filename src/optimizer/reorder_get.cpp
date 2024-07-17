bb387e7a2017a596e221978c6d28bfc0fd9515b0
pei <ds1231h@gmail.com>
2024-07-17 20:58:10 +0100

    add filter as a block

--- src/optimizer/reorder_get.cpp
+++ src/optimizer/reorder_get.cpp
@@ -8,29 +8,66 @@ unique_ptr<LogicalOperator> ReorderGet::Optimize(unique_ptr<LogicalOperator> pla
 		return std::move(plan);
 	}
 
-	// test
 	// collect all tables
 	std::map<std::pair<idx_t, idx_t>, JoinCondition> join_conds;
-	std::map<idx_t, unique_ptr<LogicalOperator>> get_ops;
+	std::map<idx_t, unique_ptr<LogicalOperator>> table_index_blocks;
 	// <table_index, card>
-	std::deque<std::pair<idx_t, idx_t>> table_order;
-	std::function<void(unique_ptr<LogicalOperator> & op)> collect_get_op;
-	collect_get_op = [&collect_get_op, &join_conds, &get_ops, &table_order, this](unique_ptr<LogicalOperator> &op) {
+	std::deque<std::pair<idx_t, idx_t>> table_card_order;
+	std::function<void(unique_ptr<LogicalOperator> & op)> collect_block;
+	collect_block = [&collect_block, &join_conds, &table_index_blocks, &table_card_order,
+	                 this](unique_ptr<LogicalOperator> &op) {
 		for (auto &child : op->children) {
-			collect_get_op(child);
 			if (LogicalOperatorType::LOGICAL_GET == child->type) {
 				auto &get_op = child->Cast<LogicalGet>();
 				auto temp_table_card = std::make_pair(get_op.table_index, get_op.EstimateCardinality(context));
-				get_ops[get_op.table_index] = std::move(child);
+				table_index_blocks[get_op.table_index] = std::move(child);
 				// sort the table index with card, from the biggest to the smallest
-				for (size_t idx = 0; idx < table_order.size(); idx++) {
-					if (table_order[idx].second < temp_table_card.second) {
-						auto temp = table_order[idx];
-						table_order[idx] = temp_table_card;
+				for (size_t idx = 0; idx < table_card_order.size(); idx++) {
+					if (table_card_order[idx].second < temp_table_card.second) {
+						auto temp = table_card_order[idx];
+						table_card_order[idx] = temp_table_card;
 						temp_table_card = temp;
 					}
 				}
-				table_order.push_back(temp_table_card);
+				table_card_order.push_back(temp_table_card);
+				continue;
+			} else if (LogicalOperatorType::LOGICAL_FILTER == child->type) {
+				// we consider the FILTER block as a whole, even it has JOINs
+				std::function<void(unique_ptr<LogicalOperator> & op)> collect_filter;
+				int table_index = -1;
+				collect_filter = [&collect_filter, &table_card_order, &table_index,
+				                  this](unique_ptr<LogicalOperator> &op) {
+					for (auto &child : op->children) {
+						if (LogicalOperatorType::LOGICAL_GET == child->type) {
+							auto &get_op = child->Cast<LogicalGet>();
+							auto temp_table_card =
+							    std::make_pair(get_op.table_index, get_op.EstimateCardinality(context));
+							// sort the table index with card, from the biggest to the smallest
+							for (size_t idx = 0; idx < table_card_order.size(); idx++) {
+								if (table_card_order[idx].second < temp_table_card.second) {
+									auto temp = table_card_order[idx];
+									table_card_order[idx] = temp_table_card;
+									temp_table_card = temp;
+								}
+							}
+							table_card_order.push_back(temp_table_card);
+#if DEBUG
+							// the FILTER should only have one table,
+							// and we ignore DATA_CHUNK since it should only have a small number of records
+							D_ASSERT(-1 == table_index);
+#endif
+							table_index = get_op.table_index;
+							continue;
+						}
+						collect_filter(child);
+					}
+				};
+				collect_filter(child);
+#if DEBUG
+				D_ASSERT(table_index != -1);
+#endif
+				table_index_blocks[table_index] = std::move(child);
+				continue;
 			} else if (LogicalOperatorType::LOGICAL_COMPARISON_JOIN == child->type) {
 				auto &join_op = child->Cast<LogicalComparisonJoin>();
 				for (auto &cond : join_op.conditions) {
@@ -47,9 +84,17 @@ unique_ptr<LogicalOperator> ReorderGet::Optimize(unique_ptr<LogicalOperator> pla
 					join_conds[std::make_pair(left_table_index, right_table_index)] = std::move(cond);
 				}
 			}
+			collect_block(child);
 		}
 	};
-	collect_get_op(plan);
+	collect_block(plan);
+
+#if ENABLE_DEBUG_PRINT
+	Printer::Print("table_card_order");
+	for (const auto &ele : table_card_order) {
+		Printer::Print("table " + std::to_string(ele.first) + " with card = " + std::to_string(ele.second));
+	}
+#endif
 
 	auto plan_pointer = plan.get();
 	// get the position before the first join
@@ -61,9 +106,9 @@ unique_ptr<LogicalOperator> ReorderGet::Optimize(unique_ptr<LogicalOperator> pla
 	std::stack<vector<JoinCondition>> join_conditions_stack;
 	std::stack<idx_t> joined_table_index;
 
-	while (!table_order.empty()) {
-		auto table_index = table_order.front().first;
-		table_order.pop_front();
+	while (!table_card_order.empty()) {
+		auto table_index = table_card_order.front().first;
+		table_card_order.pop_front();
 		vector<JoinCondition> join_conditions;
 		for (auto it = join_conds.begin(); it != join_conds.end();) {
 			bool find_in_left = false;
@@ -91,17 +136,14 @@ unique_ptr<LogicalOperator> ReorderGet::Optimize(unique_ptr<LogicalOperator> pla
 		if (join_conds.empty())
 			break;
 	}
-#ifdef DEBUG
-	D_ASSERT(!table_order.empty());
-#endif
 
 	// get the current_plan or the rest of tables
-	unique_ptr<LogicalOperator> current_plan = std::move(get_ops[table_order.back().first]);
-	table_order.pop_back();
-	while (!table_order.empty()) {
-		current_plan =
-		    LogicalCrossProduct::Create(std::move(current_plan), std::move(get_ops[table_order.back().first]));
-		table_order.pop_back();
+	unique_ptr<LogicalOperator> current_plan = std::move(table_index_blocks[table_card_order.back().first]);
+	table_card_order.pop_back();
+	while (!table_card_order.empty()) {
+		current_plan = LogicalCrossProduct::Create(std::move(current_plan),
+		                                           std::move(table_index_blocks[table_card_order.back().first]));
+		table_card_order.pop_back();
 	}
 
 	unique_ptr<LogicalOperator> tmp_comp_join;
@@ -111,7 +153,7 @@ unique_ptr<LogicalOperator> ReorderGet::Optimize(unique_ptr<LogicalOperator> pla
 #endif
 		tmp_comp_join = make_uniq<LogicalComparisonJoin>(JoinType::INNER);
 		tmp_comp_join->children.push_back(std::move(current_plan));
-		tmp_comp_join->children.push_back(std::move(get_ops[joined_table_index.top()]));
+		tmp_comp_join->children.push_back(std::move(table_index_blocks[joined_table_index.top()]));
 		joined_table_index.pop();
 		tmp_comp_join->Cast<LogicalComparisonJoin>().conditions = std::move(join_conditions_stack.top());
 		join_conditions_stack.pop();
