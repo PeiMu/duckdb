72291709da7e6d2e768a5feb92769d5b3dd8a3cd
pei <ds1231h@gmail.com>
2024-10-08 21:14:38 +0100

    upload script

--- measure/convert_postgres_plan_query_split.sh
+++ measure/convert_postgres_plan_query_split.sh
@@ -7,7 +7,7 @@ rm -f convert_postgres.log
 
 clear && cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=0 VERBOSE=1 make 2>&1|tee -a compile.log && cd measure && clear
 
-dir="/home/pei/Project/benchmarks/imdb_job-postgres/queries_without_AS"
+dir="/home/pei/Project/benchmarks/imdb_job-postgres/queries_without_AS_QuerySplit"
 iteration=1
 
 for i in $(eval echo {1.."${iteration}"}); do
