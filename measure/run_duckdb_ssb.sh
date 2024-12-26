dd9bb33e3b4d29e85569c5a39221c5cbb3dec45b
pei <ds1231h@gmail.com>
2024-12-26 14:12:06 +0000

    upload run script

---
+++ measure/run_duckdb_ssb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# execute queries
+dir="/home/pei/Project/benchmarks/ssb-skew-duckdb/ssb/queries"
+iteration=1
+
+rm -rf ssb_result/
+mkdir -p ssb_result/
+
+for i in $(eval echo {1.."${iteration}"}); do
+  for sql in "${dir}"/*; do
+    echo "execute ${sql}" 2>&1|tee -a duckdb_query_split_ssb_${i}.txt;
+    echo -ne ".read ${sql}" | duckdb ./ssb.duckdb 2>&1|tee -a duckdb_query_split_ssb_${i}.txt;
+  done
+done
+
+mv duckdb_query_split_ssb_* ssb_result/.
---
+++ measure/run_duckdb_ssb_skew.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# execute queries
+dir="/home/pei/Project/benchmarks/ssb-skew-duckdb/ssb-skew/queries/"
+iteration=1
+
+rm -rf ssb_skew_result/
+mkdir -p ssb_skew_result/
+
+for i in $(eval echo {1.."${iteration}"}); do
+  for sql in "${dir}"/*; do
+    echo "execute ${sql}" 2>&1|tee -a duckdb_query_split_ssb_skew_${i}.txt;
+    echo -ne ".read ${sql}" | duckdb ./ssb_skew.duckdb 2>&1|tee -a duckdb_query_split_ssb_skew_${i}.txt;
+  done
+done
+
+mv duckdb_query_split_ssb_skew_* ssb_skew_result/.
