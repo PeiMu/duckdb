1b7aa55ffc16a9749d46be4865210438fca36edf
pei <ds1231h@gmail.com>
2025-02-27 14:31:20 +0000

    we use single thread

--- src/include/duckdb/main/config.hpp
+++ src/include/duckdb/main/config.hpp
@@ -119,7 +119,7 @@ struct DBConfigOptions {
 	//! The maximum memory used by the database system (in bytes). Default: 80% of System available memory
 	idx_t maximum_memory = (idx_t)-1;
 	//! The maximum amount of CPU threads used by the database system. Default: all available.
-	idx_t maximum_threads = (idx_t)-1;
+	idx_t maximum_threads = 1;
 	//! The number of external threads that work on DuckDB tasks. Default: 1.
 	//! Must be smaller or equal to maximum_threads.
 	idx_t external_threads = 1;
