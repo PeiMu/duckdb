1b39b523af662ede4f76ff8a05ba3ed7b8c69393
PeiMu <ds1231h@gmail.com>
2025-06-04 17:24:02 +0100

    do not show the compile process

--- measure/measure_breakdown_time_job.sh
+++ measure/measure_breakdown_time_job.sh
@@ -11,7 +11,8 @@ rm -rf job_result/*${LOG_NAME}
 
 
 ###### official duckdb
-cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=0 ENABLE_CROSS_PRODUCT_REWRITE=0 VERBOSE=1 ENABLE_MEASURE_EXE_TIME=1 make && cd measure/
+echo "compile official duckdb"
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=0 ENABLE_CROSS_PRODUCT_REWRITE=0 VERBOSE=1 ENABLE_MEASURE_EXE_TIME=1 make > -a compile.log && cd measure/
 echo "PreOptimize, final-PostOptimize, final-CreatePlan, Execute"  >> ${LOG_NAME};
 for sql in "${dir}"/*; do
   echo "execute ${sql}" >> ${LOG_NAME};
@@ -32,7 +33,8 @@ mv ${LOG_NAME} duckdb_official_breakdown_${LOG_NAME}
 #done
 #mv ${LOG_NAME} duckdb_js_wo_stats_breakdown_${LOG_NAME}
 
-cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_SPECIFY_EST_STAT=1 ENABLE_MEASURE_EXE_TIME=1 VERBOSE=1 make && cd measure/
+echo "compile with ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_SPECIFY_EST_STAT=1 ENABLE_MEASURE_EXE_TIME=1"
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_SPECIFY_EST_STAT=1 ENABLE_MEASURE_EXE_TIME=1 VERBOSE=1 make > -a compile.log && cd measure/
 for sql in "${dir}"/*; do
   echo "execute ${sql}" >> ${LOG_NAME};
   for i in $(eval echo {1.."${iteration}"}); do
@@ -52,7 +54,8 @@ mv ${LOG_NAME} duckdb_rsj_wo_stats_breakdown_${LOG_NAME}
 #mv ${LOG_NAME} duckdb_js_whole_plan_breakdown_${LOG_NAME}
 
 ###### reorder table + split + join order opt + merge back to the whole plan
-cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_SPECIFY_EST_STAT=1 ENABLE_MERGE_BACK_PLAN=1 VERBOSE=1 make && cd measure/
+echo "compile with ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_SPECIFY_EST_STAT=1 ENABLE_MERGE_BACK_PLAN=1"
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_SPECIFY_EST_STAT=1 ENABLE_MERGE_BACK_PLAN=1 VERBOSE=1 make > -a compile.log && cd measure/
 for sql in "${dir}"/*; do
   echo "execute ${sql}" >> ${LOG_NAME};
   for i in $(eval echo {1.."${iteration}"}); do
@@ -74,7 +77,8 @@ mv ${LOG_NAME} duckdb_rsj_whole_plan_wo_stats_breakdown_${LOG_NAME}
 #mv ${LOG_NAME} duckdb_js_breakdown_${LOG_NAME}
 
 ###### reorder table + split + join order opt
-cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 VERBOSE=1 ENABLE_MEASURE_EXE_TIME=1 make && cd measure/
+echo "compile with ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_MEASURE_EXE_TIME=1"
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 VERBOSE=1 ENABLE_MEASURE_EXE_TIME=1 make > -a compile.log && cd measure/
 for sql in "${dir}"/*; do
   echo "execute ${sql}" >> ${LOG_NAME};
   for i in $(eval echo {1.."${iteration}"}); do
@@ -95,7 +99,8 @@ mv ${LOG_NAME} duckdb_rsj_breakdown_${LOG_NAME}
 #mv ${LOG_NAME} duckdb_js_whole_plan_breakdown_${LOG_NAME}
 
 ###### reorder table + split + join order opt + merge back to the whole plan
-cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_MERGE_BACK_PLAN=1 ENABLE_CROSS_PRODUCT_REWRITE=1 VERBOSE=1 make && cd measure/
+echo "compile with ENABLE_QUERY_SPLIT=1 ENABLE_MERGE_BACK_PLAN=1 ENABLE_CROSS_PRODUCT_REWRITE=1"
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_MERGE_BACK_PLAN=1 ENABLE_CROSS_PRODUCT_REWRITE=1 VERBOSE=1 make > -a compile.log && cd measure/
 for sql in "${dir}"/*; do
   echo "execute ${sql}" >> ${LOG_NAME};
   for i in $(eval echo {1.."${iteration}"}); do
--- measure/measure_job.sh
+++ measure/measure_job.sh
@@ -4,7 +4,7 @@ mkdir -p job_result/
 rm -rf compile.log
 
 echo "official" 2>&1|tee -a compile.log
-cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=0 VERBOSE=1 make 2>&1|tee -a compile.log && cd measure && bash ./hyperfine_in_mem_job.sh official nan
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=0 VERBOSE=1 make > -a compile.log && cd measure && bash ./hyperfine_in_mem_job.sh official nan
 
 # without updating statistics
 #if [ $1 == 'estimated_stats' ]
@@ -13,7 +13,7 @@ cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=0 VERBOSE=1 make 2>&1|tee -
 #echo "query_split with join_order_optimization before query_split, with estimated stats" 2>&1|tee -a compile.log
 #cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 ENABLE_SPECIFY_EST_STAT=1 VERBOSE=1 make 2>&1|tee -a compile.log && cd measure && bash ./hyperfine_in_mem_job.sh query_split js_wo_stats_stats
 
-echo "query_split with join_order_optimization after query_split, with estimated stats" 2>&1|tee -a compile.log
+echo "query_split with join_order_optimization after query_split, with estimated stats" > -a compile.log
 cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_SPECIFY_EST_STAT=1 VERBOSE=1 make 2>&1|tee -a compile.log && cd measure && bash ./hyperfine_in_mem_job.sh query_split rsj_wo_stats_stats
 ############################# estimated stats #############################
 #fi
@@ -21,7 +21,7 @@ cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWR
 #echo "query_split with join_order_optimization before query_split" 2>&1|tee -a compile.log
 #cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 VERBOSE=1 make 2>&1|tee -a compile.log && cd measure && bash ./hyperfine_in_mem_job.sh query_split js
 
-echo "query_split with join_order_optimization after query_split" 2>&1|tee -a compile.log
+echo "query_split with join_order_optimization after query_split" > -a compile.log
 cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 VERBOSE=1 make 2>&1|tee -a compile.log && cd measure && bash ./hyperfine_in_mem_job.sh query_split rsj
 
 mv compile.log job_result/.
