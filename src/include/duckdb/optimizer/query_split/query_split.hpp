a1c54471bca9ab949b36ad84087717da3f682c92
pei <ds1231h@gmail.com>
2025-06-26 11:20:17 +0100

    re open plan_reorder

--- src/include/duckdb/optimizer/query_split/query_split.hpp
+++ src/include/duckdb/optimizer/query_split/query_split.hpp
@@ -17,7 +17,7 @@
 #define MANUAL_EXPLAIN_ANALYZE     false
 #define WHOLE_PLAN_EXPLAIN_ANALYZE false
 #define ALWAYS_SPLIT               true
-#define ENABLE_REORDER_PLAN        false
+#define ENABLE_REORDER_PLAN        true
 
 #if ENABLE_MEASURE_EXE_TIME || ENABLE_MERGE_BACK_PLAN || ENABLE_DEBUG_PRINT
 inline bool execute_plan = false;
