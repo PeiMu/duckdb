ebf14c8011ecb7464112d88dc6c685afea8333e2
Pei Mu <ds1231h@gmail.com>
2025-07-20 16:11:40 +0100

    Update hyperfine_in_mem_dsb.sh

--- measure/hyperfine_in_mem_dsb.sh
+++ measure/hyperfine_in_mem_dsb.sh
@@ -5,8 +5,8 @@ log_name=duckdb_$1_$2.csv
 rm -rf ${log_name}
 rm -rf dsb_$3_result/${log_name}
 
-dir_1="/home/pei/Project/benchmarks/dsb-postgres/code/tools/1_instance_out_wo_multi_block/1/"
-dir_2="/home/pei/Project/benchmarks/dsb-postgres/code/tools/1_instance_out_wo_multi_block/2/"
+dir_1="$DSB_PATH/code/tools/1_instance_out_wo_multi_block/1/"
+dir_2="$DSB_PATH/code/tools/1_instance_out_wo_multi_block/2/"
 iteration=10
 
 for sql in $(find "$dir_1" "$dir_2" -type f -name "*.sql"); do
