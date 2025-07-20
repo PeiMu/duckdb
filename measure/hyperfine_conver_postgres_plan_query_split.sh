dfa90f3d868364ec9c66f577faf65e6bc8e1bf3e
Pei Mu <ds1231h@gmail.com>
2025-07-20 16:11:13 +0100

    Update hyperfine_conver_postgres_plan_query_split.sh

--- measure/hyperfine_conver_postgres_plan_query_split.sh
+++ measure/hyperfine_conver_postgres_plan_query_split.sh
@@ -7,15 +7,15 @@ rm -rf job_result/${log_name}
 
 clear && cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=0 VERBOSE=1 make 2>&1|tee -a compile.log && cd measure && clear
 
-#dir="/home/pei/Project/benchmarks/imdb_job-postgres/queries_without_AS_QuerySplit"
-dir="/home/pei/Project/benchmarks/imdb_job-postgres/QuerySplit/queries_new_settings_QuerySplit"
+#dir="$JOB_PATH/queries_without_AS_QuerySplit"
+dir="$JOB_PATH/QuerySplit/queries_new_settings_QuerySplit"
 iteration=10
 
 for sql in "${dir}"/*.sql; do
   echo "convert ${sql}" 2>&1|tee -a convert_postgres.log;
   psql -U imdb -d imdb -f "${sql}" 2>&1|tee -a convert_postgres.log;
   echo "hyperfine run ${sql}" 2>&1|tee -a ${log_name}
-  hyperfine --warmup 3 --runs ${iteration} -i "duckdb -c \".read ${sql}\" ./imdb.db" 2>&1|tee -a ${log_name}
+  hyperfine --warmup 5 --runs ${iteration} -i "duckdb -c \".read ${sql}\" ./imdb.db" 2>&1|tee -a ${log_name}
 done
 
 mv ${log_name} job_result/.
