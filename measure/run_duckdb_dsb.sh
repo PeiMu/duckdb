267113167b80beb4a57f4e7d9d2482196b2369f7
Pei Mu <ds1231h@gmail.com>
2025-07-20 16:16:25 +0100

    Update run_duckdb_dsb.sh

--- measure/run_duckdb_dsb.sh
+++ measure/run_duckdb_dsb.sh
@@ -10,30 +10,6 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
-#rm -f ./dsb_$2.db
-#
-#cd ../ && make clean && GEN=ninja VERBOSE=1 make 2>&1|tee -a compile.log && cd measure
-#
-## create schema
-#echo "create dsb schema"
-#echo -ne ".read create_tables.sql" | duckdb ./dsb_$2.db
-#
-## load dsb
-#for table in customer_address customer_demographics date_dim warehouse ship_mode time_dim reason income_band item store call_center customer web_site store_returns household_demographics web_page promotion catalog_page inventory catalog_returns web_returns web_sales catalog_sales store_sales 
-#do
-#  echo "duckdb load table from ${table}.tbl"
-#  if [ "$2" -eq 10 ]; then
-#    command="copy ${table} from '$DSB_PATH/code/tools/out/csv/${table}.csv' (quote '\"', escape '\\');"
-#  elif [ "$2" -eq 100 ]; then
-#    command="copy ${table} from '$DSB_PATH/code/tools/out_100/csv/${table}.csv' (quote '\"', escape '\\');"
-#  else
-#    echo "Please enter a correct scale factor 10/100, or check the csv file path!"
-#  fi
-#  echo $command
-#  echo -ne "${command}" | duckdb ./dsb_$2.db
-#done
-
-
 # execute queries
 dir_1="$DSB_PATH/code/tools/1_instance_out_wo_multi_block/1/"
 dir_2="$DSB_PATH/code/tools/1_instance_out_wo_multi_block/2/"
