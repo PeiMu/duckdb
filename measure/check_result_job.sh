b5bac383e211c8607832a59f873a8951e16daa06
pei <ds1231h@gmail.com>
2025-10-07 17:38:50 +0100

    upload measurement scripts

--- measure/check_result_job.sh
+++ measure/check_result_job.sh
@@ -6,10 +6,11 @@ cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=0 VERBOSE=1 make >> compile
 echo "query_split with join_order_optimization before query_split" 2>&1|tee -a compile.log
 cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./run_duckdb_job.sh js
 
-#echo "query_split with join_order_optimization after query_split" 2>&1|tee -a compile.log
-#cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./run_duckdb_job.sh rsj
+##echo "query_split with join_order_optimization after query_split" 2>&1|tee -a compile.log
+##cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./run_duckdb_job.sh rsj
 
 diff job_result/duckdb_job_result_official.txt job_result/duckdb_job_result_js.txt 2>&1 | tee job_js_diff.log
+#diff job_result/duckdb_job_result_official.txt job_result/duckdb_job_result_rsj.txt 2>&1 | tee job_rsj_diff.log
 
 echo "query_split with join_order_optimization before query_split" 2>&1|tee -a compile.log
 cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 ENABLE_MERGE_BACK_PLAN=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./run_duckdb_job.sh js_merge_back
@@ -18,3 +19,10 @@ cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWR
 #cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_MERGE_BACK_PLAN=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./run_duckdb_job.sh rsj_merge_back
 
 diff job_result/duckdb_job_result_official.txt job_result/duckdb_job_result_js_merge_back.txt 2>&1 | tee job_js_merge_back_diff.log
+#diff job_result/duckdb_job_result_official.txt job_result/duckdb_job_result_rsj_merge_back.txt 2>&1 | tee job_rsj_merge_back_diff.log
+
+echo "query_split with join_order_optimization before query_split" 2>&1|tee -a compile.log
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 ENABLE_SPECIFY_EST_STAT=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./run_duckdb_job.sh js
+
+#echo "query_split with join_order_optimization after query_split" 2>&1|tee -a compile.log
+#cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=1 ENABLE_SPECIFY_EST_STAT=1 VERBOSE=1 make >> compile.log 2>&1 && cd measure && bash ./run_duckdb_job.sh rsj
