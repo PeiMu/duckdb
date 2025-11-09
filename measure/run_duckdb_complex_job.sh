649aca41e85206f6b775aa35333dc2d1d354a572
pei <ds1231h@gmail.com>
2025-11-09 14:23:20 +0000

    upload job_complex script

---
+++ measure/run_duckdb_complex_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# execute queries
+dir="$JOB_COMPLEX_PATH/sql_queries"
+iteration=1
+
+log_name=duckdb_job_result_$1.txt
+
+rm -rf ${log_name}
+rm -rf job_result/${log_name}
+mkdir -p job_result/
+
+for i in $(eval echo {1.."${iteration}"}); do
+  for sql in "${dir}"/*; do
+    echo "execute ${sql}" 2>&1|tee -a ${log_name};
+    echo -ne ".read ${sql}" | duckdb ./imdb.db 2>&1|tee -a ${log_name};
+  done
+done
+
+mv ${log_name} job_result/.
