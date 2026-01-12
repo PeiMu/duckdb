057e5ebe1676891f6e892a70b5c426c0cd9c5d53
pei <ds1231h@gmail.com>
2026-01-12 17:57:32 +0000

    update scripts

--- measure/dump_job_sub_plans.sh
+++ measure/dump_job_sub_plans.sh
@@ -9,19 +9,20 @@ log_name=duckdb_job_result_$1.txt
 rm -rf ${log_name}
 rm -rf job_result/${log_name}
 mkdir -p job_result/
+mkdir -p job_result/0.10.1/
 
 # change `ENABLE_OPTIMIZER_COMPARISON` to true
-sed -i 's/#define ENABLE_OPTIMIZER_COMPARISON\s\+false/#define ENABLE_OPTIMIZER_COMPARISON true/' ../src/include/duckdb/optimizer/query_split/query_split.hpp
-cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 VERBOSE=1 make && cd measure/
+sed -i 's/#define ENABLE_OPTIMIZER_COMPARISON\s\+false/#define ENABLE_OPTIMIZER_COMPARISON true/' ${PWD}/../src/include/duckdb/optimizer/query_split/query_split.hpp
+cd ../ && make clean && GEN=ninja ENABLE_QUERY_SPLIT=1 ENABLE_CROSS_PRODUCT_REWRITE=0 VERBOSE=1 make >> compile.log 2>&1 && cd measure/
 # rest
-sed -i 's/#define ENABLE_OPTIMIZER_COMPARISON\s\+true/#define ENABLE_OPTIMIZER_COMPARISON false/' ../src/include/duckdb/optimizer/query_split/query_split.hpp
+sed -i 's/#define ENABLE_OPTIMIZER_COMPARISON\s\+true/#define ENABLE_OPTIMIZER_COMPARISON false/' ${PWD}/../src/include/duckdb/optimizer/query_split/query_split.hpp
 
 for i in $(eval echo {1.."${iteration}"}); do
   for sql in "${dir}"/*; do
     filename=${sql%/}        # remove trailing /
     filename=${filename##*/} # remove everything before last /
     id=${filename%.sql}      # remove .sql
-    rm -rf ${PWD}/job_result/${id}/
+    rm -rf ${PWD}/job_result/0.10.1/${id}/
   done
 done
 
@@ -33,8 +34,8 @@ for i in $(eval echo {1.."${iteration}"}); do
     filename=${filename##*/} # remove everything before last /
     id=${filename%.sql}      # remove .sql
     echo "sql id is: ${id}"
-    mkdir ${PWD}/job_result/${id}/
-    mv *.bin ${PWD}/job_result/${id}/
+    mkdir ${PWD}/job_result/0.10.1/${id}/
+    mv *.bin ${PWD}/job_result/0.10.1/${id}/
   done
 done
 
